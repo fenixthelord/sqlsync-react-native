11e1534e43912d06e1d0558b1d9a18890e9df3e9
Muhammad Khalaf <fenixthelord@users.noreply.github.com>
2026-06-17 23:36:38 +0300

    Export new mappings and utility functions

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ export { useSqlSync } from './hooks/useSqlSync';
 export { useSqlSyncStats } from './hooks/useSqlSyncStats';
 export { useSqlSyncRecord } from './hooks/useSqlSyncRecord';
 
+export { useSqlSyncMappings } from './hooks/useSqlSyncMappings';
+export { resolveFields, getPrices, getUnits, getFieldByRole } from './utils/mapping';
+export type { SqlSyncMapping, ResolvedField, UseSqlSyncMappingsReturn } from './types';
+
 // Provider
 export { SqlSyncProvider, useSqlSyncConfig } from './SqlSyncProvider';
 
